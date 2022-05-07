5bf75d39542c08f57b968c17e8b7aacce2ad1625
Mohammad Omar Shihab <lonak.secret@gmail.com>
2022-05-07 15:53:34 +0300

    add all mission icons

--- src/Components/wwdCard/WwdCard.js
+++ src/Components/wwdCard/WwdCard.js
@@ -1,14 +1,15 @@
 import './WwdCard.css'
-// import img from '../../assets/logo.png'
+import img from '../../assets/logo.png'
+import { getIcon } from '../../utils/iconGetter'
 
 const WwdCard = ({title,subtitle,icon,bg}) => { 
 
 return(
 
-<div className='WwdCard' style={{backgroundImage:`require(url(../.${bg}))`}}>
+<div className='WwdCard' style={{backgroundImage:`url(${bg})`}}>
     <div className="bg-overlay"></div>
     <div className="icon"
-    data-aos={`zoom-in-down`} data-aos-duration="1250">{icon}</div>
+    data-aos={`zoom-in-down`} data-aos-duration="1250">{getIcon(icon)}</div>
 <h1 data-aos={`fade-in`} data-aos-duration="1000">{title}</h1>
 <h4 data-aos={`fade-in`} data-aos-duration="3000">{subtitle}</h4>
 </div>
