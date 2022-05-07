b224395b93312cb0d149d5b665d6e31cb63fa26d
Mohammad Omar Shihab <lonak.secret@gmail.com>
2022-05-07 18:35:37 +0300

    add react context + language switcher

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import './App.css';
-import { Navbar } from "./Components"
+import { useContext } from 'react';
+import { Navbar,LngSelect } from "./Components"
 import { Hero, Services, Whatwedo, Tools, Plans, Clients, Whorwe, Contact,Footer } from "./Sections"
+import LanguageContext from './context/LanguageContext'
+
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
+
 import heLang from './languages/hebrew.json'
+import arLang from './languages/arabic.json'
 
 function App() {
+
   AOS.init({
     offset: 200,
   });
 
+const {language} = useContext(LanguageContext);
+let lng = language == 'arabic' ? arLang : heLang
 
   return (
+
     <div className="App">
-      <Navbar language={heLang} />
-      <Hero language={heLang} />
-      <Services language={heLang} />
-      <Whatwedo language={heLang} />
-      <Tools language={heLang} />
+      <LngSelect/>
+      <Navbar language={lng} />
+      <Hero language={lng} />
+      <Services language={lng} />
+      <Whatwedo language={lng} />
+      <Tools language={lng} />
       <Plans />
       <Clients />
       <Whorwe />
---
+++ src/Components/LngSelect/LngSelect.js
@@ -0,0 +1,28 @@
+import {useEffect,useContext } from 'react';
+import LanguageContext from '../../context/LanguageContext';
+import './LngSelect.css'
+
+
+const LngSelect = () => { 
+
+const {changeLang,language} = useContext(LanguageContext);
+
+useEffect(() => {
+
+console.log('langauge eff');
+console.log(language);
+
+},[language] )
+
+return(
+
+<div className='LngSelect' style={{display:`${language != ''  && 'none'}`}}
+data-aos={`zoom-in`} data-aos-duration="1250">
+<div className="arabic" onClick={() => changeLang('arabic')}>عربي</div>
+<div className="hebrew" onClick={() => changeLang('hebrew')}>עברית</div>
+</div>
+
+)
+}
+
+export default LngSelect
