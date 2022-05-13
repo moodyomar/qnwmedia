2b0fd87972ce8d271e71171ac31099bd53375103
Mohammad Omar Shihab <lonak.secret@gmail.com>
2022-05-13 15:05:37 +0300

    init clients + add assets & content

--- src/Components/ClientFavicon/ClientFavicon.js
+++ src/Components/ClientFavicon/ClientFavicon.js
@@ -1,12 +1,21 @@
 import './ClientFavicon.css'
 
 
-const ClientFavicon = () => { 
+const ClientFavicon = ({name,website,favicon,placment}) => { 
 
 return(
 
 <div className='ClientFavicon'>
-<h1>ClientFavicon</h1>
+	<div class="avatar" style={{
+left:placment.left,
+right:placment.right,
+top:placment.top,
+bottom:placment.bottom
+}}>
+		<a href={website}>
+			<img src={favicon} alt={name} />
+		</a>
+	</div>
 </div>
 
 )
--- src/Sections/Clients/Clients.js
+++ src/Sections/Clients/Clients.js
@@ -1,4 +1,4 @@
-import { SectionHeading } from '../../Components'
+import { ClientFavicon, SectionHeading } from '../../Components'
 import './Clients.css'
 
 
@@ -8,6 +8,10 @@ return(
 
 <div className='Clients flex-center section'>
 <SectionHeading p={p} h2={h2}/>
+{clients.map(client => (
+    <ClientFavicon name={client.name} website={client.website}
+    favicon={client.favicon} placment={client.placment}/>
+))}
 </div>
 
 )
