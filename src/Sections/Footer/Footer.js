e1f5f440dd851cd593db21d650a1b234ca40db2c
Mohammad Omar Shihab <lonak.secret@gmail.com>
2022-05-15 07:24:59 +0300

    init footer + add assets & content

--- src/Sections/Footer/Footer.js
+++ src/Sections/Footer/Footer.js
@@ -5,8 +5,12 @@ const Footer = () => {
 
 return(
 
-<div className='Footer'>
-<h1>Footer</h1>
+<div className='Footer cwhite'>
+<div className="footer-col1">QNW Media</div>
+<div className="footer-col2">mail section</div>
+<div className="footer-col3">location</div>
+<div className="footer-col4">services</div>
+<div className="footer-bottom">© 2020 — 2021 by <span> QNW Media.</span> All Rights Reserved.</div>
 </div>
 
 )
