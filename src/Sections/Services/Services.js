e22abf15d03055980cad6965c2ea0192c8cfde8b
Mohammad Omar Shihab <lonak.secret@gmail.com>
2022-06-01 07:30:54 +0300

    desktop responsivity

--- src/Sections/Services/Services.js
+++ src/Sections/Services/Services.js
@@ -3,38 +3,42 @@ import { AiFillCheckCircle } from 'react-icons/ai';
 import { SectionHeading, ServiceCard } from '../../Components'
 
 
-const Services = ({language}) => { 
-const {sh2,features,p,wh2,cards} = language.services;
-return(
+const Services = ({ language }) => {
+    const { sh2, features, p, wh2, cards } = language.services;
+    return (
 
-<div className='Services flex-center section' id='1'>
-<h2 className='sectionTitle'>{sh2}
-<div className="animated-title">
-<div className="word word1"></div>
-    <span>לעסק</span>
-</div>
-</h2>
+        <div className='Services flex-center section' id='1'>
+            <h2 className='sectionTitle'>{sh2}
+                <div className="animated-title">
+                    <div className="word word1"></div>
+                    <span>לעסק</span>
+                </div>
+            </h2>
 
 
-<img src={'images/qnw-instagram.png'} className="igimg" alt="QNW Media Instagram"
- data-aos="fade-right" data-aos-duration="2000" />
-<div className="features">
-    {features.map((feature,i) => (
-<span key={i} className="feature"
-data-aos="fade-left" data-aos-duration={`${feature.speed}`}>
-    {feature.title} <AiFillCheckCircle size={30}/></span>
-    ))}
-</div>
-<SectionHeading p={p} h2={wh2}/>
-<div className="cards">
-{cards.map((card,i) => (
-    <ServiceCard key={i} icon={card.icon} title={card.title}
-    desc={card.description} dir={card.dir}/>
-))}
-</div>
-</div>
+            <div className="image-and-features">
+                <img src={'images/qnw-instagram.png'} className="igimg" alt="QNW Media Instagram"
+                    data-aos="fade-right" data-aos-duration="2000" />
+                <div className="features">
+                    {features.map((feature, i) => (
+                        <span key={i} className="feature"
+                            data-aos="fade-left" data-aos-duration={`${feature.speed}`}>
+                            {feature.title} <AiFillCheckCircle size={30} /></span>
+                    ))}
+                </div>
+            </div>
 
-)
+
+            <SectionHeading p={p} h2={wh2} />
+            <div className="cards">
+                {cards.map((card, i) => (
+                    <ServiceCard key={i} icon={card.icon} title={card.title}
+                        desc={card.description} dir={card.dir} />
+                ))}
+            </div>
+        </div>
+
+    )
 }
 
 export default Services
